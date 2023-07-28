fca0e6215240f92d27b443da9caacb0f40b6c410
Paulo Lima <paulo.lima@pitang.com>
2023-07-28 17:20:25 -0300

    Refactor to class based view

--- repositories/views.py
+++ repositories/views.py
@@ -1,24 +1,25 @@
-from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework import mixins, generics
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 
-from .models import Commit
+from .models import Commit, Repository
 from .serializers import CommitSerializer, RepositorySerializer
 
 
-@api_view(["GET"])
-@permission_classes([IsAuthenticated])
-def commit_list_view(request):
-    commits = Commit.objects.all()
-    serializer = CommitSerializer(commits, many=True)
-    return Response(serializer.data)
+class CommitView(mixins.ListModelMixin, generics.GenericAPIView):
+    queryset = Commit.objects.all()
+    serializer_class = CommitSerializer
+    permission_classes = [IsAuthenticated]
 
+    def get(self, request):
+        return self.list(request)
 
-@api_view(["POST"])
-@permission_classes([IsAuthenticated])
-def repository_create_view(request):
-    serializer = RepositorySerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
+class RepositoryView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Repository.objects.all()
+    serializer_class = RepositorySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
