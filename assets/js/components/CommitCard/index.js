2c836889b78778ed9750989c7798ee8b26fd606b
Paulo Lima <paulo.lima@pitang.com>
2023-07-31 00:02:55 -0300

    fake avatar in case github commiter doesn't have a profile picture

--- assets/js/components/CommitCard/index.js
+++ assets/js/components/CommitCard/index.js
@@ -2,10 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export function CommitCard({ commit }) {
+  const getNameInitials = () => {
+    return `${commit.author.split(' ')[0].charAt(0)}${commit.author.split(' ')[1].charAt(0)}`
+  }
+
   return (
     <div>
       <div className="avatar">
-        {commit.avatar && <img alt={commit.author} className="img-author" src={commit.avatar} />}
+        {commit.avatar ? (
+          <img
+            alt={commit.author}
+            className="img-author"
+            src={commit.avatar}
+          />
+        ) : (
+          <div className='missing-avatar'>
+            <span>{getNameInitials()}</span>
+          </div>
+        )}
       </div>
 
       <div className="commit-details">
